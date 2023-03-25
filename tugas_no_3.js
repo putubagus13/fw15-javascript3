80edaaddc08e8d5f337510e1f4ed4cdc9303fe29
Putu Bagus Raditya <127736637+putubagus13@users.noreply.github.com>
2023-03-25 23:03:08 +0700

    Add files via upload

---
+++ tugas_no_3.js
@@ -0,0 +1,25 @@
+const getMonth = (callback) => {
+    setTimeout(()=>{
+        let error = false
+        let month = ['Januari','Februari', 'Maret', 'April', 'Mei',
+                    'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November',
+                    'Desember']
+                if(!error){
+                    callback(null, month)
+                }else{
+                    callback(new Error('Sorry Data Not Found'),[])
+                }
+    },4000)
+}
+
+const showMonth = (error, month) =>{
+    if(!error){
+        let result = month.map((month) => {
+            return month})
+        console.log(result)
+    }else{
+        console.log(error.message)
+    }
+}
+
+getMonth(showMonth);
